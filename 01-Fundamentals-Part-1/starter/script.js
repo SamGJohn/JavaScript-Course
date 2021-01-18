ab2aeb2c21b1030e4df8117f27e4149a0c3f6c71
Sam Johnson <samjohnson@sams-mbp-5.home>
2021-01-18 03:18:08 -0500

    3:18

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -122,6 +122,11 @@
 //////////////////////////////////////////////////
 // CODING CHALLENGE 4
 
-const bill = 430
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;  
-console.log(`the total is ${tip + bill}`)
+// const bill = 430
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;  
+// console.log(`the total is ${tip + bill}`)
+
+//////////////////////////////////////////////////
+// 30. Javascript Releases: ES%, ES6+ and ESNext
+
+//test test
