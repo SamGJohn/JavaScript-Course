8a600a7ce91c4487b88682d4ea787b2ef9ba4d2d
Sam Johnson <samjohnson@MacBook-Pro.lan>
2021-02-03 01:43:56 -0500

    February 3 1:42 AM
    Completed Coding Challenge 5.1. Super Hard. Skipped Section 6 on HTML and CSS. Finished section 7.74

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// 71. What's the DOM and DOM Manipulation
+/*
+  Document Object Model: Structured representation of HTML documents that allows JS to access HTML elements and styles to manilpulate them
+*/
+
+// 72. Selecting and Manipulating Elements
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+
+// console.log(document.querySelector('.guess').value);
+
+// 73. Handling Click Events
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  }
+});
