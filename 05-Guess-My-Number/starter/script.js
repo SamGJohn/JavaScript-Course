4ce9a074a4fd73888cf46531a1f49ec44a2c2108
Sam Johnson <samjohnson@MacBook-Pro.lan>
2021-02-11 10:23:11 -0500

    February 11 2021
    Created random number in code and DOM. Created click event listener that triggered entire game logic. Implamented game logic and limits.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -20,11 +20,52 @@
 
 // 73. Handling Click Events
 
+const secretNumber = Math.trunc(Math.random() * 10) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+  console.log(guess);
+  console.log(typeof guess);
 
+  // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number';
+    document.querySelector('.message').textContent = '⚠️ No Number!';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 You Got It!';
+
+    document.querySelector('body').style.backgroundColor = 'green';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // When Guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        '🤦‍♂️ Too High. Guess Again!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🤯 You Lost';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        '🤦 Too Low. Guess Again!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🤯 You Lost';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
+
+// this function will only be called when the event happens
+//remember that functions are just values
